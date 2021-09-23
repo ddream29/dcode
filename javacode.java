1b903d76e828b03c56914221fba67ebc1b1ff5e3
ddream29 <91258967+ddream29@users.noreply.github.com>
2021-09-23 17:41:56 +0530

    Initial Adds

---
+++ javacode.java
@@ -0,0 +1,19 @@
+//String Builder
+
+StringBuilder strb = new StringBuilder();
+
+For output -> str.toString();
+
+strb and strb.reverse if you do equal check will always return true as it changes the vales then ad there.
+  
+//Characters
+  
+Stringname.charAt(indexpositon);
+
+//Output integer Array
+
+return new int[] {1,2,3};
+
+
+  
+ 
